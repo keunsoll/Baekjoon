0017c33f68de05aa56952adc60cf4990d2030f06
Kimeunsol <sol991005@koreatech.ac.kr>
2022-06-15 21:37:15 +0900

    2490 commit

---
+++ 2490/2490_1.py
@@ -0,0 +1,13 @@
+for i in range(3):
+    n=sum(map(int, input().split()))
+
+    if n==3:
+        print("A")
+    if n==2:
+        print("B")
+    if n==1:
+        print("C")
+    if n==0:
+        print("D")
+    if n==4:
+        print("E")    
