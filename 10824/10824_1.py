bfa4b1c599c2c09a0aae079d057c8f16a96a4647
Kimeunsol <sol991005@koreatech.ac.kr>
2022-06-29 19:19:39 +0900

    10824 commit

---
+++ 10824/10824_1.py
@@ -0,0 +1,7 @@
+num=input().split()
+
+ab=int(num[0]+num[1])
+cd=int(num[2]+num[3])
+
+ans=ab+cd
+print(ans)
--- 10825/10825_1.py
+++ 10825/10825_1.py
@@ -5,9 +5,7 @@ for i in range(n):
     std.append(input().split())
 
 std.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
-
+# sort(key=lambda x: 차례대로 입력된 인자들의 인덱스 기준으로 정렬
 for i in std:
     print(i[0])
-
-
 
