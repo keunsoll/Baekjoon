b46c8b79309d1661e2051d8fb108ed1ae1d4c32f
Kimeunsol <sol991005@koreatech.ac.kr>
2022-07-25 20:57:06 +0900

    최소공배수 commit

--- 1934/1934_1.py
+++ 1934/1934_1.py
@@ -1,12 +1,25 @@
-import sys
-input=sys.stdin.readline
+# 시간초과
+# import sys
+# input=sys.stdin.readline
+# t=int(input())
+
+# for i in range(t):
+#     num=list(map(int,input().split()))
+#     for j in range(1,min(num)+1):
+#         if num[0]%j==0 and num[1]%j==0:
+#             a=j
+#     ans=num[0]*num[1]//a
+#     print(ans)    
+
+
 t=int(input())
 
 for i in range(t):
-    num=list(map(int,input().split()))
-    for j in range(1,min(num)+1):
-        if num[0]%j==0 and num[1]%j==0:
+    x,y=map(int,input().split())
+    for j in range(1,min(x,y)+1):
+        if x%j==0 and y%j==0:
             a=j
-    ans=num[0]*num[1]//a
+    ans=x*y//a
     print(ans)        
+
 
