1012490844066222f64d575d59d971d8c54c6145
Kimeunsol <sol991005@koreatech.ac.kr>
2022-08-19 22:06:21 +0900

    11866 commit

---
+++ 11866/11866_1.py
@@ -0,0 +1,17 @@
+from collections import deque
+import sys
+input=sys.stdin.readline
+
+n,k=map(int,input().split())
+c=deque([i for i in range(1,n+1)])
+print('<',end='')
+
+while c:
+    for i in range(k-1):
+        c.append(c[0])
+        c.popleft()
+    print(c.popleft(),end='')
+    if c:
+        print(', ', end='')
+print('>')        
+
