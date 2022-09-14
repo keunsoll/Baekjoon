11dcb97ebfce082c487fc02eca5087f2fbffe948
Kimeunsol <sol991005@koreatech.ac.kr>
2022-09-14 19:11:09 +0900

    dfs bfs commit

---
+++ 1260/1260_1.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+n,m,v=map(int,input().split())
+visited=[0]*(n+1)
+g=[[0]*(n+1) for i in range(n+1)]
+
+def dfs(v):
+    visited[v]=1
+    print(v, end=" ")
+    for i in range(1,n+1):
+        if(visited[i]==0 and g[v][i]==1):
+            dfs(i)
+
+def bfs(v):
+    q=deque([v])
+    visited[v]=0
+    while q:
+        v=q.popleft()
+        print(v,end=" ")
+        for i in range(1,n+1):
+            if visited[i]==1 and g[v][i]==1:
+                q.append(i)
+                visited[i]=0
+
+
+for i in range(m):
+    x,y=map(int,input().split())
+    g[x][y]=g[y][x]=1
+
+dfs(v)
+print()
+bfs(v)
+
+
+
+    
+    
